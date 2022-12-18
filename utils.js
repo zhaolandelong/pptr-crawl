e5ea8324835fda169ec22f4516117c4ec826d4cd
zhaolandelong <landelong.zhao@tusimple.ai>
2022-12-19 00:26:50 +0800

    chore: add csv2xlsx serviceDownload

--- utils.js
+++ utils.js
@@ -1,13 +1,17 @@
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+const { convertCsvToXlsx } = require("@aternus/csv-to-xlsx");
+
 exports.waitClickSelector = async (page, selector) => {
   await page.waitForSelector(selector);
   await page.click(selector);
 };
 
-exports.waitPathResponse = async (page, path) => {
-  await page.waitForResponse(
+exports.waitPathResponse = (page, path) =>
+  page.waitForResponse(
     (response) => response.url().includes(path) && response.status() === 200
   );
-};
 
 exports.delay = async (ms) => new Promise((rev) => setTimeout(() => rev(), ms));
 
@@ -29,4 +33,31 @@ exports.getCookie = async (page, key) => {
 exports.getDate = () => {
   const t = new Date();
   return `${t.getFullYear()}-${t.getMonth() + 1}-${t.getDate()}`;
+};
+
+exports.serviceDownload = (url, dist) => {
+  const service = url.startsWith("https:") ? https : http;
+  return new Promise((rev, rej) => {
+    service
+      .request(url, (res) => {
+        res.pipe(fs.createWriteStream(dist));
+
+        res.on("end", () => {
+          rev(dist);
+        });
+
+        res.on("error", (err) => {
+          rej(err);
+        });
+      })
+      .end();
+  });
+};
+
+exports.csv2xlsx = (source, dist) => {
+  let _dist = dist;
+  if (typeof _dist === "undefined") {
+    _dist = source.replace(".csv", ".xlsx");
+  }
+  convertCsvToXlsx(source, _dist);
 };
